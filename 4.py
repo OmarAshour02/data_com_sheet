339da16da99c074d5b0acb5d639b39501f44bc1f
Omar Ashour <68746440+OmarAshour02@users.noreply.github.com>
2023-12-12 15:13:52 +0200

    Update 4.py

--- 4.py
+++ 4.py
@@ -7,16 +7,32 @@ y2 = np.sin(2 * x_val)
 y3 = np.sin(4 * x_val)
 y4 = np.sin(6 * x_val)
 
-plt.plot(x_val, y1, label="sin(x)")
-plt.plot(x_val, y2, label="sin(2 * x)")
-plt.plot(x_val, y3, label="sin(4 * x)")
-plt.plot(x_val, y4, label="sin(6 * x)")
-
-plt.title('Four Signals Together')
-
+plt.subplot(4,1,1)
+plt.title('First')
+plt.stem(x_val, y1, label="sin(x)")
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
-plt.legend()
 
+plt.subplot(4,1,2)
+plt.title('Second')
+plt.stem(x_val, y2, label="sin(2 * x)")
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+
+plt.subplot(4,1,3)
+plt.title('Third')
+plt.stem(x_val, y3)
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+
+plt.subplot(4,1,4)
+plt.title('Forth')
+plt.stem(x_val, y4, label="sin(6 * x)")
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+
+
+
+plt.tight_layout()
 
 plt.show()
